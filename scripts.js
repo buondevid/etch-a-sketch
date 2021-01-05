e6248d2da91e697b6effb41bb0256588025a67aa
buondevid <davidebriano@gmail.com>
2021-01-05 03:01:00 +0100

    finish all logic, styling missing

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function createGrid(n) {
 		gridDiv.classList.add('grid-div');
 		gridWrapper.appendChild(gridDiv);
 	}
-	Array.from(allGridDiv).forEach((e) => e.addEventListener('mouseover', () => e.classList.add('black')));
+	/* Array.from(allGridDiv).forEach((e) => e.addEventListener('mouseover', () => e.classList.add('black'))); */
 }
 createGrid(16);
 
@@ -28,9 +28,9 @@ const pRange = document.getElementById('p-range');
 
 gridRange.addEventListener('mouseup', () => {
 	pRange.textContent = gridRange.value; // update Input Range p.value
-	const number = gridRange.value; // giving number user's input value
-	gridWrapper.innerHTML = '';
-	createGrid(number);
+	const number = gridRange.value; // assignin to Number user's input value
+	gridWrapper.innerHTML = '';	// delete all div
+	createGrid(number); // create new grid with user's input
 	gridWrapper.setAttribute('style', `grid-template: repeat(${number}, 1fr) / repeat(${number}, 1fr);`); // resizing grid
 });
 
@@ -38,7 +38,7 @@ function getRandom() {
 	return Math.floor(Math.random() * 256);
 }
 
-function rainbow() {
+/* function rainbow() {
 	Array.from(allGridDiv).forEach(
 		(e) => {
 			e.removeEventListener('mouseover', () => e.classList.add('black'));
@@ -46,16 +46,65 @@ function rainbow() {
 				() => e.setAttribute('style', `background-color: rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`));
 		},
 	);
-}
+} */
 
-function shades() {
+/* function shades() {
 	Array.from(allGridDiv).forEach(
 		(e) => {
 			e.removeEventListener('mouseover', () => e.classList.add('black'));
 			e.removeEventListener('mouseover',
-				() => e.setAttribute('style', `background-color: rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`))
+				() => e.setAttribute('style', `background-color: rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`));
 			e.addEventListener('mouseover',
 				() => e.setAttribute('style', `background-color: rgba(0, 0, 0, ${0.1}`));
 		},
 	);
-}
+} */
+
+let color;
+const buttons = document.querySelectorAll('.buttons');
+
+buttons.forEach((e) => e.addEventListener('click', () => {
+	color = e.textContent.toLowerCase();
+	if (color === 'rubber') {
+		Array.from(allGridDiv).forEach(
+			(grid) => {
+				grid.classList.remove('black');
+				grid.addEventListener('mouseover',
+					() => grid.setAttribute('style', 'background-color: white'));
+			},
+		);
+	} else if (color === 'rainbow') {
+		Array.from(allGridDiv).forEach(
+			(grid) => {
+				grid.classList.remove('black');
+				grid.addEventListener('mouseover',
+					() => grid.setAttribute('style', `background-color: rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`));
+			},
+		);
+	} else if (color === 'black') {
+		Array.from(allGridDiv).forEach(
+			(grid) => {
+				grid.classList.remove('black');
+				grid.addEventListener('mouseover',
+					() => grid.setAttribute('style', 'background-color: black'));
+			},
+		);
+	} else if (color === 'shades') {
+		Array.from(allGridDiv).forEach(
+			(g) => {
+				g.addEventListener('mouseover',
+					() => {
+						if (g.style.backgroundColor.match(/rgba/)) {
+							let currentOpacity = +(g.style.backgroundColor.slice(-4, -1));
+							console.log(currentOpacity);
+							if (currentOpacity <= 0.9) {
+								g.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.2})`;
+								
+								
+							}
+						} else { g.style.backgroundColor = 'rgba(0, 0, 0, 0.1)'; }
+					});
+			},
+		);
+	}
+}));
